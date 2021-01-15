bfb4a6c9f655a8d4e8d6fb5cdf7f1f144d83f8e4
everywherewego <964353527@qq.com>
2021-01-15 15:07:15 +0800

    new branch dev

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("第一步");
+        System.out.println("dev");
+
 
     }
 }
