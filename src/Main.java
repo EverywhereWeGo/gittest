ca2b619d701fb8e1de8b573aa4c5d44b8e0ceab6
everywherewego <964353527@qq.com>
2021-01-15 15:08:12 +0800

    需改maseter
    (cherry picked from commit 309d0bf36295f09cea56fef9b456166823c5a17f)

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("第一步");
-        System.out.println("dev");
-        System.out.println("修改dev");
+        System.out.println("修改master");
 
 
     }
