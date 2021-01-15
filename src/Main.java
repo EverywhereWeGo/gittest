0e524b551a0edf1875b59f943381631631276b17
everywherewego <964353527@qq.com>
2021-01-15 15:11:44 +0800

    修改dev

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("第一步");
         System.out.println("dev");
+        System.out.println("修改dev");
 
 
     }
---
+++ src/rrrr.java
@@ -0,0 +1,11 @@
+/**
+ * @author everywherewego
+ * @date 1/15/21 3:11 PM
+ */
+
+public class rrrr {
+    public static void main(String[] args) {
+        System.out.println("new eee");
+
+    }
+}
